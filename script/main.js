2bd21f968d90c23f3bf73bab613c350eeafd3aa0
Giampaolo1 <giampaolo.r@outlook.com>
2019-11-06 18:28:06 +0100

    A

--- script/main.js
+++ script/main.js
@@ -7,17 +7,19 @@
 // gli altri diventano verdi (tutti i rimanennti)
 // Opzionale: Sopra alla griglia deve esserci un contatore che conta quanti rossi e quanti verdi sono stati scoperti
 
+// Contatori
+var contgreen = 0;
+var contred = 0;
 
 $( document ).ready(function() {
 
-  // var contgreen = 0;
-  // var contred = 0;
-
-  $(".square").click(function() {
+  $(".green").click(function() {
     $(this).css("background","lightgreen");
 
-    // contgreen = +1;
+    contgreen ++;
 
+    console.log('VERDE', contgreen)
+    $("#verde").text(contgreen)
 
     // var green = true;
     //
@@ -31,16 +33,18 @@ $( document ).ready(function() {
 //       Nikolas Mosca:boolean: 4:40 PM
 // ciao! ho visto il ticket e ho visto il codice :wink: attenzione a dove dichiari le variabili di contatore e a come le incrementi :wink: ti do questo piccolo indizio, se hai bisogno possiamo cmq vederlo assieme su zoom :slightly_smiling_face: dimmi tu ok?
 
-
   });
-
-  console.log("red ",contred, "green ",contred);
-
-
 
 
   $(".red").click(function() {
     $(this).css("background","red");
+
+    contred++;
+
+    console.log('ROSSO', contred)
+
+    $("#rosso").text(contred)
+
   });
 
 
@@ -49,3 +53,19 @@ $( document ).ready(function() {
 
 
 });
+
+
+// var contgreen = 0;
+// var contred = 0;
+// $( document ).ready(function() {
+//   $(".square").click(function() {
+//     $(this).css("background","lightgreen");
+//     contgreen++;
+//     console.log('contatore verde', contgreen)
+//   });
+//   $(".red").click(function() {
+//     $(this).css("background","red");
+//     contred++;
+//     console.log('contatore rosso', contred)
+//   });
+// });
