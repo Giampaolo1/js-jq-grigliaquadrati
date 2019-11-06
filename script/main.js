f8af151d08026040b338f4870c39f6fb3da2737b
Giampaolo1 <giampaolo.r@outlook.com>
2019-11-06 17:54:15 +0100

    last push

--- script/main.js
+++ script/main.js
@@ -10,22 +10,32 @@
 
 $( document ).ready(function() {
 
+  // var contgreen = 0;
+  // var contred = 0;
+
   $(".square").click(function() {
-    $(this).css("background","lightgreen")
+    $(this).css("background","lightgreen");
 
-    var contgreen = 0;
-    var contred = 0;
-    var green = true;
+    // contgreen = +1;
 
-    if (green == true) {
-      contgreen = +1;
-       } else {
-      contred = +1;
-      }
 
-    console.log("red ",contred, "green ",contred);
-    
+    // var green = true;
+    //
+    // if (green == true) {
+    //   contgreen = +1;
+    //   }
+      // else {
+      // contred = +1;
+      // }
+
+//       Nikolas Mosca:boolean: 4:40 PM
+// ciao! ho visto il ticket e ho visto il codice :wink: attenzione a dove dichiari le variabili di contatore e a come le incrementi :wink: ti do questo piccolo indizio, se hai bisogno possiamo cmq vederlo assieme su zoom :slightly_smiling_face: dimmi tu ok?
+
+
   });
+
+  console.log("red ",contred, "green ",contred);
+
 
 
 
